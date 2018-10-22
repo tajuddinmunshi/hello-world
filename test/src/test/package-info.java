31cfbb563f48da72d9d616fee769e8684a039c11
Ligin.George <geoligin@gmail.com>
2018-10-22 13:03:30 +0400

    pushing latest

---
+++ test/src/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Ligin.George
+ *
+ */
+package test;
---
+++ test/src/test/testclass.java
@@ -0,0 +1,12 @@
+package test;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello Workd");
+
+	}
+
+}
